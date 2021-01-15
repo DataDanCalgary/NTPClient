474d58290c82d4a3daf41b96dae2bcc70ca7d77e
DataDanCalgary <dan.henne@gmail.com>
2021-01-15 10:50:58 -0700

    Update NTPClient.cpp

--- NTPClient.cpp
+++ NTPClient.cpp
@@ -209,38 +209,9 @@ String NTPClient::getFormattedDate() {
 }
 
 String NTPClient::getFormattedUTC() { 
-  unsigned long rawDays = this->getEpochTime() / 86400L;  // in days
-  unsigned long days = 0, year = 1970;
-  uint8_t month;
-  static const uint8_t monthDays[]={31,28,31,30,31,30,31,31,30,31,30,31};
 
-  unsigned long rawTime = this->getEpochTime();
-  unsigned long hours = (rawTime % 86400L) / 3600;
-  String hoursStr = hours < 10 ? "0" + String(hours) : String(hours);
+  return getFormattedDate() + "T" + getFormattedTime() + "Z";
 
-  unsigned long minutes = (rawTime % 3600) / 60;
-  String minuteStr = minutes < 10 ? "0" + String(minutes) : String(minutes);
-
-  unsigned long seconds = rawTime % 60;
-  String secondStr = seconds < 10 ? "0" + String(seconds) : String(seconds);
-
-  while((days += (LEAP_YEAR(year) ? 366 : 365)) <= rawDays) 
-    year++;
-  rawDays -= days - (LEAP_YEAR(year) ? 366 : 365); // now it is days in this year, starting at 0
-  days=0;
-  for (month=0; month<12; month++) {
-    uint8_t monthLength;
-    if (month==1) { // february
-      monthLength = LEAP_YEAR(year) ? 29 : 28;
-    } else {
-      monthLength = monthDays[month];
-    }
-    if (rawDays < monthLength) break;
-    rawDays -= monthLength;
-  }
-  String monthStr = ++month < 10 ? "0" + String(month) : String(month); // jan is month 1  
-  String dayStr = ++rawDays < 10 ? "0" + String(rawDays) : String(rawDays); // day of month
-  return String(year) + "-" + monthStr + "-" + dayStr + "T" + hoursStr + ":" + minuteStr + ":" + secondStr + "Z";  //getFormattedTime() + 
 }
 
 void NTPClient::end() {
